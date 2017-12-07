20cbd0715b0abc5dd47c6c3be804ba45f3332c21
Adriana Pereira <adripereira.adriana@gmail.com>
2017-12-07 14:11:29 +0000

    some imports added, some functions changed, naive bayes rolling - ex3 stage 3.0

--- ex2/priors.py
+++ ex2/priors.py
@@ -1,10 +1,11 @@
+import numpy as np
 def uniform_prior(sent_index,graph,sentences):
     return 1/len(sentences)
 
 #receives graph matrix for convenience...
 def degree_centrality_prior(sent_index,graph,sentences):
 	total_links = 0
-	degree = 0 
+	degree = 0
 	for i in range(len(graph)):
 		nonzeros = len(np.nonzero(graph[i])[0])
 		if i == sent_index:
